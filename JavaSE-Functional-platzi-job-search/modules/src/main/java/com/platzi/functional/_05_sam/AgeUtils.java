5819f8813772aa9f279e8434f90609684982994a
cantolinMeli <claudio.antolin@mercadolibre.cl>
2022-07-22 23:55:48 -0400

    added explanatory comments on the class AgeUtils

--- JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_05_sam/AgeUtils.java
+++ JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_05_sam/AgeUtils.java
@@ -7,10 +7,16 @@ import java.util.function.Function;
 public class AgeUtils {
 
     public static void main(String[] args) {
+        //Función que antepone un cero a cualquier valor menor de 10
         Function<Integer, String> addCeros = x -> x < 10 ? "0" + x : String.valueOf(x);
+
+        //Función que hereda del método de la interface. Parsea a date una fecha ingresada como String
         TriFunction<Integer, Integer, Integer, LocalDate> parseDate =
                 (day, month, year) -> LocalDate.parse(year + "-" +addCeros.apply(month) + "-" + day);
 
+        /*Función que hereda del método de la interface.
+        Calcula la diferencia en años entre la fecha ingresada y la fecha de hoy
+         */
         TriFunction<Integer, Integer, Integer, Integer> calculateAge =
                 (day, month, year) -> Period.between(
                         parseDate.apply(day, month, year), LocalDate.now()
